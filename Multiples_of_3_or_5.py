3dfddba94188041044bf0b80cc4cf7e51e03a358
Ashwin2583 <esashwin04@gmail.com>
2025-02-28 01:02:37 +0530

    The program is completed

--- Multiples_of_3_or_5.py
+++ Multiples_of_3_or_5.py
@@ -1,1 +1,26 @@
-import numpy as np
+
+num = int(input("Enter the number: "))
+def arithmetic_sum(n,a,l):
+    S_n = (n/2)*(a + l)
+    return S_n
+
+def multiples(x,m):
+    mult = []
+    i = 1
+    while i < x:
+        if i % m == 0:
+            mult.append(i)
+        i = i + 1
+    return mult
+
+M_3 = multiples(num,3)
+M_5 = multiples(num,5)
+M_15 = multiples(num,15)
+s_3 = arithmetic_sum((num-1)//3,M_3[0],M_3[len(M_3)-1])
+s_5 = arithmetic_sum((num-1)//5,M_5[0],M_5[len(M_5)-1])
+s_15 = arithmetic_sum((num-1)//15,M_15[0],M_15[len(M_15)-1])
+
+S = s_3 + s_5 - s_15
+print("The Sum of all the multiples of 3 or 5 below 1000 ", S)
+
+
