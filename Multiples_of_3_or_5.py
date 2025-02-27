965620b33584a0311d318e4ce485f2cbb57f23da
Ashwin2583 <esashwin04@gmail.com>
2025-02-28 01:25:40 +0530

    Added comments

--- Multiples_of_3_or_5.py
+++ Multiples_of_3_or_5.py
@@ -1,9 +1,13 @@
 
+# inputing the number 
 num = int(input("Enter the number: "))
+
+# funtion to find the arithmetic sum
 def arithmetic_sum(n,a,l):
     S_n = (n/2)*(a + l)
     return S_n
 
+# function to generate the multiples
 def multiples(x,m):
     mult = []
     i = 1
@@ -13,9 +17,9 @@ def multiples(x,m):
         i = i + 1
     return mult
 
-M_3 = multiples(num,3)
-M_5 = multiples(num,5)
-M_15 = multiples(num,15)
+M_3 = multiples(num,3) # list of multiples of 3 below num
+M_5 = multiples(num,5) # list of multiples of 5 below num
+M_15 = multiples(num,15) # list of multiples of 15 below num
 s_3 = arithmetic_sum((num-1)//3,M_3[0],M_3[len(M_3)-1])
 s_5 = arithmetic_sum((num-1)//5,M_5[0],M_5[len(M_5)-1])
 s_15 = arithmetic_sum((num-1)//15,M_15[0],M_15[len(M_15)-1])
