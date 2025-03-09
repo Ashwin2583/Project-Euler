dccec7ec5e831e3ca68fbcdcc44d82738a979566
Ashwin2583 <esashwin04@gmail.com>
2025-03-09 23:41:45 +0530

    modified special_pythagorean_triplet.py

--- special_pythagorean_triplet.py
+++ special_pythagorean_triplet.py
@@ -1,8 +1,5 @@
-for a in range(1, 333):
-    for b in range(a+1,500):
-        c = 1000 - (a + b)
-        if a**2 + b**2 == c**2:
+for a in range(1, 333): # iterating a where a<b<c
+    for b in range(a+1,500): # iterating b where b > a
+        c = 1000 - (a + b) 
+        if a**2 + b**2 == c**2: # checking for pythagorean relation
             print("The product abc: ",a*b*c)
-
-
-
